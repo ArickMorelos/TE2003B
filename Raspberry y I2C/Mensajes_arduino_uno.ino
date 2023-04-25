f85d6cf6c8e9601d73196881cb5c19c18d0f0354
ArickMorelos <127910374+ArickMorelos@users.noreply.github.com>
2023-04-24 21:04:40 -0600

    Create Mensajes_arduino_uno.ino

---
+++ Raspberry y I2C/Mensajes_arduino_uno.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  Serial.println("mexicanos"); //imprime el mensaje
+  Serial.println("al grito"); //imprime el mensaje
+  Serial.println("de guerra");  //imprime el mensaje
+  Serial.println(" El acero aprestad"); //imprime el mensaje
+  Serial.println("y el bridon");  //imprime el mensaje
+  Serial.println("y retiemble en su centro la tierra"); //imprime el mensaje
+  Serial.println("al sonoro rugir del cannion");  //imprime el mensaje
+  delay (4000);  //crea un delay de 4000 milisegundos
